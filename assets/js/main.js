cc08a83a9c6aac744d9c1507c18d6ede93fd61c5
hyejin kim <hyejin_kim@nexon.co.kr>
2019-09-11 16:22:24 +0900

    Revert "header 메뉴 효과 수정 (js)"
    This reverts commit cef4b96565973d19e3610ea0e20f0a6381ab9284.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ $(function () {
 
   // Menu Ul Settings
   $('.menu-main li').click(function (e) {
-    $('.menu-sub', this).toggleClass('active');
+    $('.menu-sub', this).toggle();
   })
 
   // Click menu
